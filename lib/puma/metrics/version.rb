52283b230f6de69694c8fc7c4aeb6f0a2a733f8a
Harm-Jan Blok <Harm-Jan.Blok@swisscom.com>
2019-10-30 16:44:47 +0100

    Set target version to 2.6

--- lib/puma/metrics/version.rb
+++ lib/puma/metrics/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Puma
   module Metrics
-    VERSION = '1.0.3'.freeze
+    VERSION = '1.0.3'
   end
 end
--- lib/puma/plugin/metrics.rb
+++ lib/puma/plugin/metrics.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'puma/metrics/dsl'
 
 Puma::Plugin.create do
